88d66705e14f7218dd5618338948019ab17ed08b
Jakob-Gulaid <japea002@osloskolen.no>
2022-09-22 08:35:18 +0200

    :exclamation: Endre riktig svar på spørsmål

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let sporsmal = [
         alt3: "en feit bil",
         alt4: "et menneskenavn",
         alt5: "neste spørsmål",
-        riktig: 1,
+        riktig: 2,
         svart: null,
     },
     {
